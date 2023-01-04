a7c530bf0874e2981a42d6ca66127b5b8b8fb69c
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-01-04 19:53:16 +0200

    Fixed the formatting.

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
-
     DemoUtils demo = new DemoUtils();
 
     @Test
     void testEqualsAndNotEquals(){
-
         assertEquals(6,demo.add(3,3));
         assertNotEquals(5,demo.add(2,-3));
     }
