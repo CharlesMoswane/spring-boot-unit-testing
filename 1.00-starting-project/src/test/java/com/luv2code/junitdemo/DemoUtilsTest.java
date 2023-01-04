6b1e824106b2ab4b15eec149467b23202d8e278d
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-01-04 19:52:47 +0200

    Moved the object instantiation in the test class.

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
+
+    DemoUtils demo = new DemoUtils();
+
     @Test
     void testEqualsAndNotEquals(){
-        DemoUtils demo = new DemoUtils();
 
         assertEquals(6,demo.add(3,3));
         assertNotEquals(5,demo.add(2,-3));
