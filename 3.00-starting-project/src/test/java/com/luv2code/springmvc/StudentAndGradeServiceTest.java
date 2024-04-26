8ec55196f4b91adacefca20df37c62c211203889
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-26 17:10:00 +0200

    feat: Implement createStudentService test method in StudentAndGradeServiceTest

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -1,8 +1,11 @@
 package com.luv2code.springmvc;
 
+import com.luv2code.springmvc.models.CollegeStudent;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestPropertySource("/application.properties")
 @SpringBootTest
@@ -11,5 +14,10 @@ public class StudentAndGradeServiceTest {
     @Test
     public void createStudentService() {
 
+        studentService.createStudent("Charles", "Moswane", "charles.moswane@luv2code_school.com");
+
+        /*CollegeStudent student = studentDao.findByEmailAddress("charles.moswane@luv2code_school.com");
+
+        assertEquals("charles.moswane@luv2code_school.com", student.getEmailAddress(), "find by email");*/
     }
 }
