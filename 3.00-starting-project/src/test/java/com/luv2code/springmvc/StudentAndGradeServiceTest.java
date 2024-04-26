13e51508d3e7c02287aa3226b94fc33c79d61320
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-26 17:40:00 +0200

    feat: Add Spring annotations to StudentAndGradeService and update test class for dependency injection

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -3,6 +3,7 @@ package com.luv2code.springmvc;
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class StudentAndGradeServiceTest {
 
+    @Autowired
     private StudentAndGradeService studentService;
 
     @Test
--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -1,5 +1,11 @@
 package com.luv2code.springmvc.service;
 
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class StudentAndGradeService {
     public void createStudent(String name, String surname, String email) {
     }
