2a7e15eccff5dbb4d382c2b91a1bc230e753b5da
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-26 17:00:00 +0200

    feat: Enhance StudentAndGradeServiceTest with SpringBootTest and initial test method

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -1,8 +1,15 @@
 package com.luv2code.springmvc;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @TestPropertySource("/application.properties")
+@SpringBootTest
 public class StudentAndGradeServiceTest {
 
+    @Test
+    public void createStudentService() {
+
+    }
 }
