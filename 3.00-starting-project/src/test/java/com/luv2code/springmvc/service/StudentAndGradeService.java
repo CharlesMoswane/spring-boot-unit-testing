6a31d2a86dd6bcd2c6bf1019a88bf3f9408ecb4c
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-26 17:50:00 +0200

    feat: Add implementation for createStudent method in StudentAndGradeService

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -1,5 +1,6 @@
 package com.luv2code.springmvc.service;
 
+import com.luv2code.springmvc.models.CollegeStudent;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -7,6 +8,9 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class StudentAndGradeService {
-    public void createStudent(String name, String surname, String email) {
+    public void createStudent(String firstname, String lastname, String emailAddress) {
+        CollegeStudent student = new CollegeStudent(firstname, lastname, emailAddress);
+        student.setId(0);
+        studentDao.save(student);
     }
 }
