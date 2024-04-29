53df5413cd8c966f74bbe06e76f0b24984ea9e56
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-29 17:30:00 +0200

    feat: Add @Autowired annotation to StudentAndGradeService for dependency injection

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -3,6 +3,7 @@ package com.luv2code.springmvc.service;
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.repository.StudentDao;
 import org.apache.catalina.Store;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,6 +11,8 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class StudentAndGradeService {
+
+    @Autowired
     private StudentDao studentDao;
 
     public void createStudent(String firstname, String lastname, String emailAddress) {
