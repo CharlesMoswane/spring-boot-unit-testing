58eb17f6d14d82fef6ad433677ed7b4daec9cb11
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-29 17:10:00 +0200

    feat: Add StudentDao class and integrate it into StudentAndGradeService

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -1,6 +1,8 @@
 package com.luv2code.springmvc.service;
 
 import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
+import org.apache.catalina.Store;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -8,6 +10,8 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class StudentAndGradeService {
+    private StudentDao studentDao;
+
     public void createStudent(String firstname, String lastname, String emailAddress) {
         CollegeStudent student = new CollegeStudent(firstname, lastname, emailAddress);
         student.setId(0);
