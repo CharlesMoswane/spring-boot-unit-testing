14ce0ac29b7635e69601ec5f3a45f69de7445330
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-05-22 17:50:00 +0200

    test: Add JdbcTemplate and MockMvc to GradebookControllerTest

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -1,11 +1,24 @@
 package com.luv2code.springmvc;
 
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 
 @TestPropertySource("/application.properties")
 @AutoConfigureMockMvc
 @SpringBootTest
 public class GradebookControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private StudentAndGradeServiceTest studentAndGradeServiceTest;
 }
