bad1ced8cd164377bc43935eda022d89226efce2
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-05-22 17:40:00 +0200

    test: Enhance GradebookControllerTest with Spring Boot configuration

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/GradebookControllerTest.java
@@ -1,7 +1,11 @@
 package com.luv2code.springmvc;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @TestPropertySource("/application.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
 public class GradebookControllerTest {
 }
