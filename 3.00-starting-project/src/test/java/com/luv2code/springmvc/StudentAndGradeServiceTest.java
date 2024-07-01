8c4e36556a2b980b28942380196d1db0bc8d64d7
CharlesMoswane <dev.moswane@gmail.com>
2024-07-01 17:10:00 +0200

    feat: Add deleteStudentService test method in StudentAndGradeServiceTest

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.jdbc.Sql;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -50,6 +51,11 @@ public class StudentAndGradeServiceTest {
         assertTrue(studentService.checkIfStudentIsNull(1));
 
         assertFalse(studentService.checkIfStudentIsNull(0));
+    }
+
+    @Test
+    public void deleteStudentService() {
+
     }
 
     @Sql("/insertData.sql")
