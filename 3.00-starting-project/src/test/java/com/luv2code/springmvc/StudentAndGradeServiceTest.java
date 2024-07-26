e5ad5ebb0f727a64db74a2b3b8d1748596fd3d44
CharlesMoswane <dev.moswane@gmail.com>
2024-07-26 17:40:00 +0200

    feat: Add ScienceGrade dependency to StudentAndGradeServiceTest for grade retrieval

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -2,6 +2,7 @@ package com.luv2code.springmvc;
 
 import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.models.MathGrade;
+import com.luv2code.springmvc.models.ScienceGrade;
 import com.luv2code.springmvc.repository.MathGradeDao;
 import com.luv2code.springmvc.repository.StudentDao;
 import com.luv2code.springmvc.service.StudentAndGradeService;
@@ -93,7 +94,7 @@ public class StudentAndGradeServiceTest {
 
 //        Get all grades with studentId
         Iterable<MathGrade> mathGrades = mathGradeDao.findGradeByStudentId(1);
-
+        Iterable<ScienceGrade> scienceGrades = scienceGradeDao.findGradeByStudentId(1);
 
 
 //        Verify there is grades
