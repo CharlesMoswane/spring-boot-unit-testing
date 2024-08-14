047b5bab4d6423a99b774a976949abfb590cb676
CharlesMoswane <dev.moswane@gmail.com>
2024-08-14 17:10:00 +0200

    feat: Add method to retrieve history grades by student ID in HistoryGradesDao

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/repository/HistoryGradesDao.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/repository/HistoryGradesDao.java
@@ -4,4 +4,6 @@ import com.luv2code.springmvc.models.HistoryGrade;
 import org.springframework.data.repository.CrudRepository;
 
 public interface HistoryGradesDao extends CrudRepository<HistoryGrade, Integer> {
+
+    public Iterable<HistoryGrade> findGradeByStudentId(int id);
 }
