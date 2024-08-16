50ace9c319458831d01c606353e1c7743b1ed7b6
CharlesMoswane <dev.moswane@gmail.com>
2024-08-16 17:30:00 +0200

    test: Add case to verify createGrade fails for invalid subject in StudentAndGradeServiceTest

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -118,6 +118,7 @@ public class StudentAndGradeServiceTest {
         assertFalse(studentService.createGrade(105, 1, "math"));
         assertFalse(studentService.createGrade(-5, 1, "math"));
         assertFalse(studentService.createGrade(80.50, 2, "math"));
+        assertFalse(studentService.createGrade(80.50, 1, "literature"));
     }
 
     @AfterEach
