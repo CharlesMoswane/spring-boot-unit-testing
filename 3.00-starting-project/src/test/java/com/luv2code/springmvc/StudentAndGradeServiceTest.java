0eab7380f961be0b211c1ee561087f9af4d6b0a9
CharlesMoswane <dev.moswane@gmail.com>
2024-08-19 17:50:00 +0200

    test: Reset science_grade ID sequence in setupAfterTransaction method

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -136,5 +136,6 @@ public class StudentAndGradeServiceTest {
 
         jdbc.execute("ALTER TABLE student ALTER COLUMN ID RESTART WITH 1");
         jdbc.execute("ALTER TABLE math_grade ALTER COLUMN ID RESTART WITH 1");
+        jdbc.execute("ALTER TABLE science_grade ALTER COLUMN ID RESTART WITH 1");
     }
 }
