5515432077910d612eb3d08b59b98f0f905284ce
CharlesMoswane <dev.moswane@gmail.com>
2024-08-26 17:10:00 +0200

    refactor: Improve readability of assertion message in deleteGradeService test

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -130,7 +130,8 @@ public class StudentAndGradeServiceTest {
 
     @Test
     public void deleteGradeService() {
-        assertEquals(1, studentService.deleteGrade(1, "math"), "Returns student id after delete");
+        assertEquals(1, studentService.deleteGrade(1, "math"), "Returns student id" +
+                " after delete");
     }
 
     @AfterEach
