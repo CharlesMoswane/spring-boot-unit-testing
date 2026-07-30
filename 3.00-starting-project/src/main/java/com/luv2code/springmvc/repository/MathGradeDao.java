aeefe534226efdff039cbcd0050e64b05b6b17d2
CharlesMoswane <dev.moswane@gmail.com>
2026-07-30 18:35:33 +0200

    feat: Add findGradeByStudentId method to MathGradeDao for retrieving grades by student ID

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/repository/MathGradeDao.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/repository/MathGradeDao.java
@@ -4,4 +4,6 @@ import com.luv2code.springmvc.models.MathGrade;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MathGradeDao extends CrudRepository<MathGrade, Integer> {
+
+    public Iterable<MathGrade> findGradeByStudentId(int id);
 }
